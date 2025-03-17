26b0f08ebeb5c2ded5628f262038532f836e6f1e
balajimamidipaka <rockingbala656@gmail.com>
2025-03-17 19:21:53 +0530

    added more info activity

--- app/src/main/java/com/balaji/mytraveljournal/HomeFragment.kt
+++ app/src/main/java/com/balaji/mytraveljournal/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.balaji.mytraveljournal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class HomeFragment : Fragment() {
 
@@ -13,6 +15,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view= inflater.inflate(R.layout.fragment_home, container, false)
+        val moreinfobutton=view.findViewById<ImageView>(R.id.more_info_view)
+        moreinfobutton.setOnClickListener {
+            val intent=Intent(requireContext(),CompleteJournalView::class.java)
+            startActivity(intent)
+        }
+        return view
     }
 }
